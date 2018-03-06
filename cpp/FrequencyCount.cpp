1efd5375a01c239799544abc00632a984eafdbf7
AshaAgrawal07 <ashaa2@illinois.edu>
2018-03-06 14:47:20 -0600

    rewriting to use a delimited to split the words as i am reading it from the file

--- cpp/FrequencyCount.cpp
+++ cpp/FrequencyCount.cpp
@@ -11,13 +11,15 @@
 #include <list>
 #include <regex>
 #include <stdexcept>
-
+#include <locale>
 using namespace std;
 
 int main() {
 
     //dictionary will contain the word as the key and its frequency as the value
     map<string, int> dictionary;
+    locale loc;
+    string delimiter = "/[]':;<>?!-_=+*@3$%^()~` ";
 
     //read the words in the file
     string word;
@@ -27,17 +29,33 @@ int main() {
     //go through each word of the file and put it into the dictionary
     while (file >> word) {
         stream >> word;
+        //split each word over interword punctuation (-, ', etc) and store into a vector
+        //if there is nothing to split over, then the vector will only hold the word itself
+        stringstream ss(word);
+        istream_iterator<string> begin(ss);
+        istream_iterator<string> end;
+        vector<string> splittedWords(begin, end);
 
-        //first check if word exists in the dictionary
-        // if it does, the increment value; else add key and value of 1 to dicitonary
-        map<string, int>::iterator it;
-        it = dictionary.find(word);
-        dictionary[word]++;
-        if (it == dictionary.end()) {
-            //if the word doesnt already exist in the dictionary, then a new K,V is made, with V=0, so I need to increment it again
-            dictionary[word]++;
+        //for each splittedword, we want to do the following:
+        for (auto &word  : splittedWords) {
+
+            //consider everything as case insensitive, so all words will be converted to lower case
+            string wordToLower = "";
+            for (int i = 0; i < word.length(); i++) {
+                wordToLower += tolower(word[i], loc);
+            }
+            //first check if word exists in the dictionary
+            // if it does, the increment value; else add key and value of 1 to dicitonary
+            map<string, int>::iterator it;
+            it = dictionary.find(wordToLower);
+            dictionary[wordToLower]++;
+            if (it == dictionary.end()) {
+                //if the word doesnt already exist in the dictionary, then a new K,V is made, with V=0, so I need to increment it again
+                dictionary[wordToLower]++;
+            }
         }
     }
+
 
     //after getting all of the words and their frequencies, I want to sort from highest frequency to lowest
     //i used this link:  https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value
@@ -51,5 +69,14 @@ int main() {
     });
 
     //now I want to output the top 10 words
+    int n = 10;
+    if (pairs.size() < 10) {
+        n = pairs.size();
+    }
+    for(int i = 0; i < n; i++) {
+        cout << "Word:  "<< pairs[i].first << "  Frequency:  " << pairs[i].second << endl;
+
+    }
+    file.close();
 }
 
