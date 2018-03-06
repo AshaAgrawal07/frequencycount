fe73c8018674e730e38000646bff2d53ee427bed
AshaAgrawal07 <ashaa2@illinois.edu>
2018-03-06 17:48:51 -0600

    changed it so that i have a regex that helps me split each line of a file into words

--- cpp/FrequencyCount.cpp
+++ cpp/FrequencyCount.cpp
@@ -12,6 +12,7 @@
 #include <regex>
 #include <stdexcept>
 #include <locale>
+
 using namespace std;
 
 int main() {
@@ -19,31 +20,29 @@ int main() {
     //dictionary will contain the word as the key and its frequency as the value
     map<string, int> dictionary;
     locale loc;
-    string delimiter = "/[]':;<>?!-_=+*@3$%^()~` ";
+    regex reg("\\s+");
 
     //read the words in the file
-    string word;
-    ifstream file;
-    stringstream stream(word);
+    string file;
+    vector<string> lines;
+    while (cin) {
+        getline(cin, file);
+        lines.push_back(file);
+    }
+    for (int i = 0; i < lines.size(); i++) {
 
-    //go through each word of the file and put it into the dictionary
-    while (file >> word) {
-        stream >> word;
-        //split each word over interword punctuation (-, ', etc) and store into a vector
-        //if there is nothing to split over, then the vector will only hold the word itself
-        stringstream ss(word);
-        istream_iterator<string> begin(ss);
-        istream_iterator<string> end;
-        vector<string> splittedWords(begin, end);
+        //split each line over non-alphabetical (-, ', " " etc) and store into a vector
+        sregex_token_iterator iter(lines[i].begin(), lines[i].end(), reg, -1);
+        sregex_token_iterator end;
 
-        //for each splittedword, we want to do the following:
-        for (auto &word  : splittedWords) {
-
+        vector<string> words(iter, end);
+        //go through each word of the file and put it into the dictionary
+        for (int j = 0; j < words.size(); j++) {
+            //for each splittedword, we want to do the following:
             //consider everything as case insensitive, so all words will be converted to lower case
             string wordToLower = "";
-            for (int i = 0; i < word.length(); i++) {
-                wordToLower += tolower(word[i], loc);
-            }
+            wordToLower += tolower(words[j], loc);
+
             //first check if word exists in the dictionary
             // if it does, the increment value; else add key and value of 1 to dicitonary
             map<string, int>::iterator it;
@@ -54,17 +53,16 @@ int main() {
                 dictionary[wordToLower]++;
             }
         }
+
     }
-
-
+    
     //after getting all of the words and their frequencies, I want to sort from highest frequency to lowest
     //i used this link:  https://stackoverflow.com/questions/5056645/sorting-stdmap-using-value
     vector<pair<string, int>> pairs;
     for (auto itr = dictionary.begin(); itr != dictionary.end(); ++itr)
         pairs.push_back(*itr);
 
-    sort(pairs.begin(), pairs.end(), [=](pair<int, int>& a, pair<int, int>& b)
-    {
+    sort(pairs.begin(), pairs.end(), [=](pair<int, int> &a, pair<int, int> &b) {
         return a.second < b.second;
     });
 
@@ -73,10 +71,10 @@ int main() {
     if (pairs.size() < 10) {
         n = pairs.size();
     }
-    for(int i = 0; i < n; i++) {
-        cout << "Word:  "<< pairs[i].first << "  Frequency:  " << pairs[i].second << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Word:  " << pairs[i].first << "  Frequency:  " << pairs[i].second << endl;
 
     }
-    file.close();
 }
+
 
